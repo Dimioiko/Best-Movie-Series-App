02ab005015c2b7d0b372087dbdb6cf13be8a19f4
dimitris <doikonomou@upgrade.net.gr>
2022-09-12 16:29:50 +0300

    Add: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import MoviewNowPlaying from './components/pages/MoviesNowPlaying';
 
 function App() {
-  return <div className='App'>test</div>;
+  // https://api.themoviedb.org/3/movie/550?api_key=bce37414a95aed7c096dbb318f4b1fe1
+  return (
+    <div className='App'>
+      <NavBar />
+      <MoviewNowPlaying />
+    </div>
+  );
 }
 
 export default App;

40807ad5c0442bad76e183c25999d9732b602e33
dimitris <doikonomou@upgrade.net.gr>
2022-09-28 16:23:59 +0300

    Remove extra field from navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,31 +2,13 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 
 export default function NavBar() {
   return (
-    <AppBar position='static'>
-      <Toolbar sx={{ backgroundColor: 'black' }} variant='dense'>
-        <Typography
-          variant='h6'
-          color='inherit'
-          component='div'
-          sx={{ padding: 2 }}
-        >
+    <AppBar position="static">
+      <Toolbar sx={{ backgroundColor: 'black' }} variant="dense">
+        <Typography variant="h6" color="inherit" component="div" sx={{ padding: 2 }}>
           Movies
         </Typography>
-        <Typography
-          variant='h6'
-          color='inherit'
-          component='div'
-          sx={{ padding: 2 }}
-        >
+        <Typography variant="h6" color="inherit" component="div" sx={{ padding: 2 }}>
           Series
-        </Typography>
-        <Typography
-          variant='h6'
-          color='inherit'
-          component='div'
-          sx={{ padding: 2 }}
-        >
-          Photos
         </Typography>
       </Toolbar>
     </AppBar>

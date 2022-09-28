25fb3895dcda9888678a4b4104cba170a97f302f
dimitris <doikonomou@upgrade.net.gr>
2022-09-28 16:24:49 +0300

    Add: function onSuccess  for load more movies

--- src/components/hooks/useFetch.tsx
+++ src/components/hooks/useFetch.tsx
@@ -1,25 +1,26 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = <T,>(url: string) => {
+export const useFetch = <T,>(url: string, onSuccess: (res: T) => void) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<T>();
-  const [error, setError] = useState();
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const json = await response.json();
-
         setData(json);
+        if (onSuccess) onSuccess(json);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (e: any) {
-        setError(e);
+        setError('Something went Wrong');
       } finally {
         setLoading(false);
       }
     };
     fetchData();
-  }, [url]);
+  }, [onSuccess, url]);
 
-  return { data, loading };
+  return { data, loading, error };
 };

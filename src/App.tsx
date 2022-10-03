0550f07d1758ea537da0b40e098f3758d362c86a
dimitris <doikonomou@upgrade.net.gr>
2022-10-03 14:14:09 +0300

    Add: Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
-import MoviewNowPlaying from './components/pages/movies/MoviesNowPlaying';
-import PopularSeries from './components/pages/series/PopularSeries';
-import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/shared/NavBar';
+import Router from './routes/Router';
 
 function App() {
   // https://api.themoviedb.org/3/movie/550?api_key=bce37414a95aed7c096dbb318f4b1fe1
   return (
     <div className="App">
       <NavBar />
-
-      <Routes>
-        <Route path="/" element={<MoviewNowPlaying />} />
-        <Route path="PopularSeries" element={<PopularSeries />} />
-      </Routes>
+      <Router />
     </div>
   );
 }
